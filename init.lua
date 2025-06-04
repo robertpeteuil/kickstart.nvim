eb706d97cef5740e25d974db8130d431280fa454
Robert Peteuil <robert@peteuil.com>
2025-06-04 09:55:27 -0700

    chore: cleanup

--- init.lua
+++ init.lua
@@ -175,13 +175,18 @@ vim.o.confirm = true
 
 -- PERSONAL OPTIONS
 --
--- set indent = 4 spaces, per: https://stackoverflow.com/questions/234564/tab-key-4-spaces-and-auto-indent-after-curly-braces-in-vim
+-- set indent = 4 spaces
+--   https://stackoverflow.com/questions/234564/tab-key-4-spaces-and-auto-indent-after-curly-braces-in-vim
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 vim.lsp.enable 'bashls'
+
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>', { desc = 'Toggle Tree [E]xplorer' })
+
+-- END PERSONAL
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -192,8 +197,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>', { desc = 'Toggle Tree [E]xplorer' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
