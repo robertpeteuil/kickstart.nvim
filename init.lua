a9f087381ccbd9a2d8676ab81e5e4674d7ab27ef
Robert Peteuil <robert@peteuil.com>
2025-06-05 16:52:02 -0700

    chore: adjust lazygit keybinding

--- init.lua
+++ init.lua
@@ -323,24 +323,24 @@ require('lazy').setup({
     end,
   },
   {
-    "kdheepak/lazygit.nvim",
+    'kdheepak/lazygit.nvim',
     lazy = true,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
     },
     -- optional for floating window border decoration
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      'nvim-lua/plenary.nvim',
     },
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+      { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
