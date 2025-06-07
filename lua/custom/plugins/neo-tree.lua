b15d71dd18b543e55fd36ff605d60e916246bdf4
Robert Peteuil <robert@peteuil.com>
2025-06-07 13:45:46 -0700

    chore: move neo-tree settings to opt from setup

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -11,39 +11,35 @@ return {
   ---@module "neo-tree"
   ---@type neotree.Config?
   opts = {
-    -- fill any relevant options here
-  },
-  config = function()
-    require('neo-tree').setup {
-      -- default_component_configs = {},
-      filesystem = {
-        filtered_items = {
-          visible = false, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_hidden = true, -- only works on Windows for hidden files/directories
-          hide_by_name = {
-            --"node_modules"
-          },
-          hide_by_pattern = { -- uses glob style patterns
-            --"*.meta",
-            --"*/src/*/tsconfig.json",
-          },
-          always_show = { -- remains visible even if other settings would normally hide it
-            --".gitignored",
-          },
-          always_show_by_pattern = { -- uses glob style patterns
-            --".env*",
-          },
-          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-            '.DS_Store',
-            --"thumbs.db"
-          },
-          never_show_by_pattern = { -- uses glob style patterns
-            --".null-ls_*",
-          },
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          --"node_modules"
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        always_show_by_pattern = { -- uses glob style patterns
+          --".env*",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          '.DS_Store',
+          --"thumbs.db"
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
         },
       },
-    }
-  end,
+    },
+    use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+    -- instead of relying on nvim autocmd events.
+  },
 }
